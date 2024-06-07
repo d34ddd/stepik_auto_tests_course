f3807d05d49d47f414c1e176a95850ad7c0694e2
d34ddd <nikita.shurankov@yandex.by>
2024-06-07 23:39:52 +0300

    /

--- Painting.py
+++ Painting.py
@@ -11,3 +11,6 @@ def draw_rectangle(x, y, widht, height):
         turtle.forward(height)
         turtle.left(90)
     turtle.end_fill()
+    turtle.mainloop()
+if __name__ == '__main__':  # именно из этой точки начинает выполнение алгоритма
+    main()

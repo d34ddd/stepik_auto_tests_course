0f364fbebe50b0554b32c4b9744dc1a458437064
d34ddd <nikita.shurankov@yandex.by>
2024-06-07 23:35:42 +0300

    ver 1

---
+++ Painting.py
@@ -0,0 +1,13 @@
+import turtle
+
+def draw_rectangle(x, y, widht, height)
+    turtle.goto(x, y)
+    turtle.pencolor('blue')
+    turtle.fillcolor('black')
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.forward(widht)
+        turtle.left(90)
+        turtle.forward(height)
+        turtle.left(90)
+    turtle.end_fill()

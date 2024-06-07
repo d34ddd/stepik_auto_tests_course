d3a707e4aeb4f8406dc1d669f80bdb45233dfdfa
d34ddd <nikita.shurankov@yandex.by>
2024-06-07 23:22:14 +0300

    Last version

--- Youtube_downloader.py
+++ Youtube_downloader.py
@@ -1,9 +1,12 @@
+import time
 from pytube import YouTube 
 
-video_url = input ()
-save_location = './'
 
-yt= YouTube(video_url)
+video_url = input ("Кидай ссылку епта")
+save_location = "D:\загрузки"
+
+yt = YouTube(video_url)
 
 stream = yt.streams.filter (res='720p', progressive=True).first()
 stream.download(output_path=save_location)
+time.sleep(10)

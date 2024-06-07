6f0465ac2e292224d4118b7dbc5bf4b6813e5364
d34ddd <nikita.shurankov@yandex.by>
2024-06-07 23:42:20 +0300

    last

--- Painting.py
+++ Painting.py
@@ -1,14 +1,75 @@
-import turtle
+import turtle  # импортируем модуль для рисования "черепаха"
 
-def draw_rectangle(x, y, widht, height):
-    turtle.goto(x, y)
-    turtle.pencolor('blue')
-    turtle.fillcolor('black')
-    turtle.begin_fill()
-    for i in range(2):
-        turtle.forward(widht)
-        turtle.left(90)
-        turtle.forward(height)
-        turtle.left(90)
-    turtle.end_fill()
-    turtle.mainloop()
+# функция рисования прямоугольника
+def draw_rectangle(x, y, width, height):  # имя функции и принимаемые для обработки переменные
+    """Рисуем прямоугольник"""
+    turtle.goto(x, y)  # перемещаем курсор на позицию x, y
+    turtle.pencolor('red')  # определяем цвет контура рисования
+    turtle.fillcolor('red')  # определяем цвет заливки
+    turtle.begin_fill()  # приступить к заполнению
+    for i in range(2):  # цикл от 1-го до 2-х
+        turtle.forward(width)   # Двигаться вперед (пикселей)
+        turtle.left(90)  # повернуть налево (пикселей)
+        turtle.forward(height)  # Двигаться вперед (пикселей)
+        turtle.left(90)  # повернуть налево (пикселей)
+    turtle.end_fill()  # завершить заполнение, заливает указанные линии цветом
+
+# функция рисования звезды
+def draw_star(x, y, radius):
+    """Рисуем пятиконечную звезду"""
+    turtle.setpos(x, y)  # определяем место курсора 1-я точка пятиконечной звезды
+    pos1 = turtle.pos()  # запоминаем 1-ю точку пятиконечной звезды
+    turtle.circle(-radius, 72)  # рисуем окружность (радиус, угол)
+    pos2 = turtle.pos()  # запоминаем 2-ю точку пятиконечной звезды
+    turtle.circle(-radius, 72)  # рисуем окружность (радиус, угол)
+    pos3 = turtle.pos()  # запоминаем 3-ю точку пятиконечной звезды
+    turtle.circle(-radius, 72)  # рисуем окружность (радиус, угол)
+    pos4 = turtle.pos()  # запоминаем 4-ю точку пятиконечной звезды
+    turtle.circle(-radius, 72)  # рисуем окружность (радиус, угол)
+    pos5 = turtle.pos()  # запоминаем 5-ю точку пятиконечной звезды
+    turtle.color('yellow', 'yellow')  # определяем цвет звезды контур и заливка
+    turtle.begin_fill()  # приступить к заполнению, далее рисуем линии между вершинами звезды
+    turtle.goto(pos3)  # перемещаемся (координаты)
+    turtle.goto(pos1)  # перемещаемся (координаты)
+    turtle.goto(pos4)  # перемещаемся (координаты)
+    turtle.goto(pos2)  # перемещаемся (координаты)
+    turtle.goto(pos5)  # перемещаемся (координаты)
+    turtle.end_fill()  # завершить заполнение, заливает указанные линии цветом
+
+# главная функция
+def main():
+    """Основная программа"""
+    turtle.speed(12)  # определяем скорость
+    turtle.penup()  # поднимаем курсор
+    x, y = -270, -180  # задаем координаты
+
+    # Рисуем основную часть флага
+    width, height = 540, 360  # задаем ширину и высоту флага
+    draw_rectangle(x, y, width, height)  # запускаем функцию рисования флага, в которую передаем начальные
+    # координаты, ширину и высоту
+
+    # Рисуем большую звезду
+    pice = 22  # задаем размер одной части в 22 пикселя
+    center_x, center_y = x + 5 * pice, y + height - pice * 5  # задаем координаты центра звезды
+    turtle.goto(center_x, center_y)  # перемещаем курсор в центр звезды
+    turtle.left(90)  # поворот налево 90 пикселей
+    turtle.forward(pice * 3)  # движемся вперед 3 размера
+    turtle.right(90)  # поворот направо 90 пикселей
+    draw_star(turtle.xcor(), turtle.ycor(), pice * 3)  # запускаем функцию рисования звезды
+
+
+    x_poses, y_poses = [10, 12, 12, 10], [2, 4, 7, 9]  # создаем два списка координат по оси х и у
+
+    # Рисуем 4 маленьких звездочки
+    for x_pos, y_pos in zip(x_poses, y_poses):  # перебираем значения списка координат
+        turtle.goto(x + x_pos * pice, y + height - y_pos * pice)  # перемещаем курсор
+        turtle.left(turtle.towards(center_x, center_y) - turtle.heading())  # поворот налево 90 пикселей
+        turtle.forward(pice)    # движемся вперед 1 размер
+        turtle.right(90)  # поворот направо 90 пикселей
+        draw_star(turtle.xcor(), turtle.ycor(), pice)  # Запускаем функцию рисования звезды
+    turtle.ht()  # убираем\прячем курсор для рисования (черепаху)
+    turtle.mainloop()  # Показать окно рисования
+
+
+if __name__ == '__main__':  # именно из этой точки начинает выполнение алгоритма
+    main()  # запуск главной функции
